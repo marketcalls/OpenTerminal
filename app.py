bfdbc64f70c6cd95317a641358f501d6706d55a4
marketcalls <rajandran@marketcalls.in>
2024-10-07 20:22:19 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# Route for homepage
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Route for about page
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Run the app
+if __name__ == "__main__":
+    app.run(debug=True)
